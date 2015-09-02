97f40e937012ee86b4f802ed9bf7451cad5f8b33
Hebert Douglas <hebertdougl@gmail.com>
2015-09-02 00:32:08 -0300

    initialized the functionality to assign grades.

--- plugins/work_assignment/controllers/work_assignment_plugin_myprofile_controller.rb
+++ plugins/work_assignment/controllers/work_assignment_plugin_myprofile_controller.rb
@@ -5,25 +5,29 @@ helper CmsHelper
 
 before_filter :protect_if, :only => [:edit_visibility]
 
-def edit_visibility
-  unless params[:article_id].blank?
-    folder = profile.environment.articles.find_by_id(params[:article_id])
-    @back_to = url_for(folder.parent.url)
-    unless params[:article].blank?
-      folder.published = params[:article][:published]
-      unless params[:q].nil?
-        folder.article_privacy_exceptions = params[:q].split(/,/).map{|n| environment.people.find n.to_i}
+  def edit_visibility
+    unless params[:article_id].blank?
+      folder = profile.environment.articles.find_by_id(params[:article_id])
+      @back_to = url_for(folder.parent.url)
+      unless params[:article].blank?
+        folder.published = params[:article][:published]
+        unless params[:q].nil?
+          folder.article_privacy_exceptions = params[:q].split(/,/).map{|n| environment.people.find n.to_i}
+        end
+        folder.save!
+        redirect_to @back_to
       end
-      folder.save!
-      redirect_to @back_to
-    end    
-  end
- end
+    end
+   end
 
   def search_article_privacy_exceptions
     arg = params[:q].downcase
     result = profile.members.find(:all, :conditions => ['LOWER(name) LIKE ?', "%#{arg}%"])
     render :text => prepare_to_token_input(result).to_json
+  end
+
+  def assign_grade
+    @submission = UploadedFile.find params[:submission]
   end
 
   protected
