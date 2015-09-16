35c62f497a99fbc890dafce4858bbc05c6135c9b
Hebert Douglas <hebertdougl@gmail.com>
2015-09-16 16:28:24 -0300

    Validated the field of signup reason.
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>
    Signed-off-by: Filipe Ribeiro <firibeiro77@live.com>
    Signed-off-by: Hebert Douglas <hebertdougl@gmail.com>
    Signed-off-by: Omar Junior <omarroinuj@gmail.com>

--- plugins/sign_up_reason/lib/ext/moderate_user_registration.rb
+++ plugins/sign_up_reason/lib/ext/moderate_user_registration.rb
@@ -2,8 +2,10 @@ require_dependency 'moderate_user_registration'
 
 class ModerateUserRegistration
 
+  attr_accessible :signup_reason
+
   def information
-    { :message => _('%{sender} wants register. Signup Reason: %{signup_reason}'),
+    { :message => _('%{sender} wants register. Signup reason: %{signup_reason}'),
       :variables => {:sender => sender, :signup_reason => signup_reason} }
   end
 end
--- plugins/sign_up_reason/lib/sign_up_reason_plugin.rb
+++ plugins/sign_up_reason/lib/sign_up_reason_plugin.rb
@@ -10,26 +10,48 @@ class SignUpReasonPlugin < Noosfero::Plugin
 
   def signup_extra_contents
     proc {
-      labelled_form_field(_('Reason:'), text_area(:task, :signup_reason, :rows => 10))
+      content_tag(:div, labelled_form_field(_('Reason:'), text_area(:task, :signup_reason, :rows => 10)))
     }
   end
 
   def account_controller_filters
-    block = proc do
+    validate_block = proc do
       if request.post?
-        @user = environment.users.find_by_login(params[:user][:login])
-        @task = ModerateUserRegistration.new
-        @task.signup_reason = params[:task][:signup_reason]
-        @task.requestor_id = @user.id
-        @task.status = 4
-        @task.save!
+        if params[:task][:signup_reason].empty?
+          @person = Person.new(params[:profile_data])
+          @person.environment = environment
+          @user = User.new(params[:user])
+          @task = ModerateUserRegistration.new
+          @task.signup_reason = params[:task][:signup_reason]
+          @person.errors.add(:signup_reason, _(' invalid.'))
+          render :action => :signup
+        end
       end
     end
 
-    [{ :type => 'after_filter',
+    block = proc do
+      if request.post?
+        @user = environment.users.find_by_login(params[:user][:login])
+        if @user
+          @task = ModerateUserRegistration.new
+          @task.signup_reason = params[:task][:signup_reason]
+          @task.requestor_id = @user.id
+          @task.status = 4
+          @task.save
+        end
+      end
+    end
+
+    [
+      {:type => 'before_filter',
+      :method_name => 'validate_signup_reason',
+      :options => {:only => 'signup'},
+      :block => validate_block},
+      {:type => 'after_filter',
       :method_name => 'create_task_with_signup_reason',
       :options => {:only => 'signup'},
-      :block => block }]
+      :block => block}
+    ]
   end
 
 
