42a8c875cfd81d95fdfc5c43765a2477443457d0
Hebert Douglas <hebertdougl@gmail.com>
2015-10-30 18:20:47 -0200

    Allowed the creation of a work_assignment without a group.
    Signed-off-by: Hebert Douglas <hebertdougl@gmail.com>

--- plugins/work_assignment/lib/work_assignment_plugin.rb
+++ plugins/work_assignment/lib/work_assignment_plugin.rb
@@ -22,8 +22,8 @@ class WorkAssignmentPlugin < Noosfero::Plugin
     parent_id = context.params[:parent_id] if context.respond_to?(:params) && context.params
     if context.respond_to?(:profile) && context.profile.organization?
       parent = parent_id ? context.profile.articles.find(parent_id) : nil
-      types << WorkAssignmentPlugin::WorkAssignmentGroup if !parent
-      types << [WorkAssignmentPlugin::WorkAssignment] if parent.kind_of?(WorkAssignmentPlugin::WorkAssignmentGroup)
+      types << [WorkAssignmentPlugin::WorkAssignment]
+      types << WorkAssignmentPlugin::WorkAssignmentGroup unless parent
     end
 
     types
