0014c029a3ba3413ac2a2309a363cb986352f926
Hebert Douglas <hebertdougl@gmail.com>
2015-11-12 09:16:28 -0200

    Creating unitary test missing.
    Signed-off-by: Hebert Douglas <hebertdougl@gmail.com>

--- plugins/work_assignment/test/unit/work_assingment_plugin/work_assignment_group_test.rb
+++ plugins/work_assignment/test/unit/work_assingment_plugin/work_assignment_group_test.rb
@@ -1,11 +1,13 @@
-require File.expand_path(File.dirname(__FILE__) + "/../../../../../test/test_helper")
+require File.expand_path(File.dirname(__FILE__) + "/../../test_helper")
 
 class WorkAssignmentGroupTest < ActiveSupport::TestCase
 
   def setup
     @organization = fast_create(Organization)
-    @work_assignment_group = WorkAssignmentPlugin::WorkAssignmentGroup.create!(:name => 'Work Assignment Group', :profile => @organization, :start_date => Time.now, :end_date => Time.now + 2.day)
-    @work_assignment = WorkAssignmentPlugin::WorkAssignment.create!(:name => 'Sample Work Assignment', :profile => @organization, :begining => Time.now, :ending => Time.now + 1.day)
+    start_date = Time.now
+    end_date = Time.now + 2.day
+    @work_assignment_group = create_work_assignment_group('Work Assignment Group', @organization, start_date, end_date)
+    @work_assignment = create_work_assignment('Sample Work Assignment', @organization, nil, nil, Time.now, Time.now + 1.day)
   end
 
   should 'return children work assignment' do
@@ -13,4 +15,29 @@ class WorkAssignmentGroupTest < ActiveSupport::TestCase
     @work_assignment.save!
     assert_equal [@work_assignment], @work_assignment_group.work_assignment_list
   end
+
+  should 'not be expired if today is between start and end dates' do
+    assert_equal false, @work_assignment_group.expired?
+  end
+
+  should 'be expired if today is not between start and end dates' do
+    @work_assignment_group.start_date = Time.now - 2.day
+    @work_assignment_group.end_date = Time.now - 1.second
+    @work_assignment_group.save
+
+    assert_equal true, @work_assignment_group.expired?
+  end
+
+  should 'return status open if today is between start and end dates' do
+    assert_equal 'open', @work_assignment_group.status
+  end
+
+  should 'return status expired if today is not between start and end dates' do
+    @work_assignment_group.start_date = Time.now - 2.day
+    @work_assignment_group.end_date = Time.now - 1.second
+    @work_assignment_group.save
+
+    assert_equal 'expired', @work_assignment_group.status
+  end
+
 end
